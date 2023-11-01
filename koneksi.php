97b81a88321df64d84c4983ac192724ee1bde08c
friska <friskasasti2803@gmail.com>
2023-11-01 09:15:15 +0700

    UTS

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$koneksi = mysqli_connect("localhost","root","","keuangan");
+
+//Check connection
+if (mysqli_connect_errno()){
+	echo "konneksi database gagal:".
+	mysqli_connect_error();
+}else{
+	echo "hore bisa konek" ;
+}
+?>
